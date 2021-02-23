8396675169c7f42592b577061ef3c7553a0c3092
Acho <34348788+acho88@users.noreply.github.com>
2021-02-23 17:39:55 +0200

    add new fixes + edit modal

--- templates/assets/js/main.js
+++ templates/assets/js/main.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
+
     $("#toggleNav").click(function(){
         $("#mySidenav").toggleClass("toggled");
     });
+
     $('.btn-green').on('click', function () {
         $('.btn-green.active').toggleClass("active");;
     });
+
     $('#tab_selector').on('change', function () {
         $('.nav-tabs a').eq($(this).val()).tab('show');
     });
-    $(".date-input").datepicker("dd-mm-yy");
 
+    $(".date-input").datepicker("dd-mm-yy");
+        
+    if ($(window).width() < 992) {
+        $("#right-side .tab-pane").removeClass("active");
+        $('#right-side .tab-pane .close').on('click', function () {
+            $("#right-side .tab-pane.active").removeClass("active");
+            
+        });
+    }
 });
