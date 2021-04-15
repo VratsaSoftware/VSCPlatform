86a4fe98991652b9f71354acbbfa104714b11090
georgi2003 <georgidim30@gmail.com>
2021-04-15 12:35:32 +0300

    Fixing the site

--- resources/views/course/module/lections/show.blade.php
+++ resources/views/course/module/lections/show.blade.php
@@ -8,28 +8,27 @@
             <h2 class="text-l1">Лекция 1</h2>
         </div>
         <div class="col-auto pe-4">
-            <div class="data1">10.09.2020</div>
+            <div class="data1">10.09.2020</div> 
         </div>
         <div class="col-auto align-self-stretch border d-none d-lg-block"></div>
         <div class="col pe-4 ps-4 d-none d-lg-block">
-            <div class="data1">10.09.2020</div>
+            <div class="data1">10.09.2020</div> 
         </div>
         <div class="col-auto pe-5 d-none d-lg-block">
             <div class="pill1 d-flex align-items-center float-right rounded-circle overflow-hidden">
                 <a class="btn px-2 col p-0 text-center" id="toggleNav">
-                    <img src="{{ asset('assets/img/arrow.svg') }}" class="arrow1">
+                    <img src="{{ asset('assets/img/arrow.svg') }}"class="arrow1">
                 </a>
             </div>
         </div>
         <div class="col-auto pe-4 d-none d-lg-block">
             <div class="pill2 d-flex align-items-center float-right rounded-circle overflow-hidden">
                 <a class="btn px-2 col p-0 text-center" id="toggleNav">
-                    <img src="{{ asset('assets/img/arrow.svg') }}" class="arrow1">
+                    <img src="{{ asset('assets/img/arrow.svg') }}"class="arrow1">
                 </a>
             </div>
         </div>
     </div>
-
     <div class="video-upload row g-0 my-4 position-relative">
         <iframe width="762" height="375" src="https://www.youtube.com/embed/l8l_n-ben-g" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="border-radius: 45px;"></iframe>
     </div>
@@ -39,7 +38,7 @@
     <div class="row g-0 align-items-lg-center lh-1 pb-5">
         <div class="col-lg col-6 mb-lg-0 mb-3 text-lg-end d-block d-sm-none">
             <div class="row g-0 mt-2">
-                <div class="col-lg col-auto text-small align-self-end pe-3">Презентация</div>
+                <div class="col-lg col-auto text-small align-self-end pe-3">Презентация</div> 
                 <div class="col-auto">
                     <a href="">
                         <img src="{{ asset('assets/img/download.svg') }}">
@@ -56,7 +55,7 @@
                 <div class="col text-small text-start pe-3 d-lg-block d-none">Добави</div>
                 <div class="col-auto mx-lg-0 mx-auto">
                     <div class="d-inline-block border d-lg-block d-none">
-                        <img src="{{ asset('assets/img/plus.svg') }}">
+                        <img src="{{ asset('assets/img/plus.svg') }}" alt="">
                     </div>
                     <div class="d-lg-none btn-plus">
                         +
@@ -68,7 +67,7 @@
             <div class="row g-0">
                 <div class="col-lg col-6 mb-lg-0 mb-3 text-lg-end">
                     <div class="row g-0">
-                        <div class="col-lg col-auto text-small align-self-end pe-3">Файл1</div>
+                        <div class="col-lg col-auto text-small align-self-end pe-3">Файл1</div> 
                         <div class="col-auto">
                             <a href="">
                                 <img src="{{ asset('assets/img/download.svg') }}">
@@ -78,7 +77,7 @@
                 </div>
                 <div class="col-lg col-6 mb-lg-0 mb-3 text-lg-end">
                     <div class="row g-0">
-                        <div class="col-lg col-auto text-small align-self-end pe-3">Файл2</div>
+                        <div class="col-lg col-auto text-small align-self-end pe-3">Файл2</div> 
                         <div class="col-auto">
                             <a href="">
                                 <img src="{{ asset('assets/img/download.svg') }}">
@@ -88,7 +87,7 @@
                 </div>
                 <div class="col-lg col-6 mb-lg-0 mb-3 text-lg-end d-none d-lg-block">
                     <div class="row g-0">
-                        <div class="col-lg col-auto text-small align-self-end pe-3">Файл3</div>
+                        <div class="col-lg col-auto text-small align-self-end pe-3">Файл3</div> 
                         <div class="col-auto">
                             <a href="">
                                 <img src="{{ asset('assets/img/download.svg') }}">
@@ -104,7 +103,7 @@
             <button class="ms-xxl-2 mt-xxl-0 mt-3 btn-edit row g-0 align-items-center">
                 <div class="col text-start fw-bold">Изтрий лекция</div>
                 <div class="col-auto">
-                    <img src="{{ asset('assets/img/Delete.svg') }}">
+                    <img src="{{ asset('assets/img/Delete.svg') }}" alt="">
                 </div>
             </button>
         </div>
@@ -124,14 +123,14 @@
         <div class="col-auto ps-3 pe-3 text-normal text-uppercase d-lg-none" style="color: #8de219;">
             (24)
         </div>
-        <div class="col-auto ps-3 d-lg-none ">
+        <div class="col-auto ps-3 d-lg-none">
             <div class="row g-0">
                 <div class="col text-white deadline-2">
                     Краен срок
-                    за домашно:
+                    за домашно: 
                 </div>
                 <div class="col-auto">
-                    <div class=" d-flex align-items-center data-07 ps-4">
+                    <div class="d-flex align-items-center data-07 ps-4">
                         <div class="w-100 text-center fw-bold enddata">07.05</div>
                     </div>
                 </div>
@@ -142,7 +141,7 @@
             <div class="row g-0">
                 <div class="col text-white deadline-2">
                     Краен срок
-                    за домашно:
+                    за домашно: 
                 </div>
                 <div class="col-auto">
                     <div class=" d-flex align-items-center data-07 ps-4">
@@ -153,7 +152,7 @@
         </div>
         <div class="col-auto mx-lg-0 mx-auto d-lg-none">
             <button class="ms-xxl-2 mt-xxl-0 mt-4 btn-view-1 btn-green row g-0 align-items-center ">
-                <div class="col-auto mx-auto fw-bold see-all">Виж всички <img src="{{ asset('assets/img/action_icon.svg') }}"></div>
+                <div class="col-auto mx-auto fw-bold see-all">Виж всички <img src="{{ asset('assets/img/action_icon.svg') }}" alt=""></div>
             </button>
         </div>
         <div class="col-auto ps-3 text-normal text-uppercase text-white d-none d-lg-block">
@@ -166,7 +165,7 @@
             <div class="row g-0">
                 <div class="col ps-4 text-white deadline-2">
                     Краен срок<br>
-                    за домашно:
+                    за домашно: 
                 </div>
                 <div class="col-auto">
                     <div class="date-pill d-flex align-items-center data-07">
@@ -179,7 +178,7 @@
             <div class="row g-0">
                 <div class="col ps-3 text-white deadline-2">
                     Краен срок<br>
-                    за домашно:
+                    за домашно: 
                 </div>
                 <div class="col-auto">
                     <div class="date-pill d-flex align-items-center data-07">
@@ -216,7 +215,7 @@
                 <button class="ms-xxl-2 mt-xxl-0 mt-3 btn-edit row g-0 align-items-center">
                     <div class="col text-start fw-bold">Редактирай лекция</div>
                     <div class="col-auto">
-                        <img src="{{ asset('assets/img/edit.svg') }}">
+                        <img src="{{ asset('assets/img/edit.svg') }}" >
                     </div>
                 </button>
             </div>
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,35 +1,29 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="description" content="">
+    <meta name="author" content="">
     <title>@yield('title')</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- favicon -->
     <link rel="shortcut icon" type="image/png" href="{{asset('/images/vso-png.png')}}" />
-    <!-- facebook -->
-    <meta property="og:url" content="" />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Враца Софтуер Общество" />
-    <meta property="og:description" content="Развиваме дигитална индустрия във Враца" />
-    <meta property="og:image" content="{{asset('/images/vso-logo-bg-original.png')}}" />
-    <meta name="Description" content="Author: VSC 2018">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Fonts and Icons -->
     <link href="{{ asset('assets/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
     <!-- CSS Files -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+    <link rel= "stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="row g-0 p-lg-0 px-4 py-4 mt-lg-0 mt-2" style="margin-right: calc(var(--bs-gutter-x)/ -2);">
-		<div class="col-xl-auto pe-xl-0 pe-lg-5">
-			<div class="row g-0" style="margin-right: calc(var(--bs-gutter-x)/ -2);">
-                <!-- JQuery -->
-                <script type="text/javascript" src="{{ asset('/js/jquery.min.js') }}"></script>
+    <div class="row g-0 p-lg-0 px-4 py-4 mt-lg-0 mt-2">
+        <div class="col-xl-auto pe-xl-0 pe-lg-5">
+            <div class="row g-0">
                 <!-- nav menu -->
                 @include('navigation-left-bar')
                 <!-- nav menu END -->
@@ -37,17 +31,6 @@
                 <!-- Content START -->
                 @yield('content')
                 <!-- Content END -->
-
-                <script>
-                    $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
-                        options.async = true;
-                    });
-                </script>
-                <!-- <script src="{{asset('/js/fixed-left-top-menu.js')}}"></script>
-                <script src="{{asset('/js/edit-showing-pencil.js')}}"></script>
-                <script src="{{asset('/js/slide-alerts.js')}}"></script> -->
-                <!-- //preview picture before saving -->
-                <!-- <script src="{{asset('/js/profile-picture-preview.js')}}" charset="utf-8" async></script> -->
             </div>
         </div>
     </div>
@@ -56,40 +39,4 @@
     <script src="{{ asset('assets/js/main.js') }}" type="text/javascript"></script>
     <script type= "text/javascript" src= "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 </body>
-
-<script type="text/javascript">
-    // $(function() {
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/bootstrap-grid.min.css')}}" />');
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/font-awesome.min.css')}}" />');
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/bootstrap.css')}}" />');
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/public_profile.css')}}" />');
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/personal_profile.css')}}" />');
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/personal_application_results.css')}}" />');
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/personal_events.css')}}" />');
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/lecturer_profile.css')}}" />');
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/create_course.css')}}" />');
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/create_level.css')}}" />');
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/lecturer_course_options.css')}}" />');
-    //     $('head').append('<link rel="stylesheet" href="{{asset('/css/lecturer_courses.css')}}" />');
-    // });
-</script>
-<script>
-    // $('.download-stats').on('click', function(){
-    //     var table = 'forms';
-    //     var uri = 'data:application/vnd.ms-excel;base64,'
-    //
-    //     , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
-    //
-    //     , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
-    //
-    //     , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
-    //
-    //     var table = document.getElementById(table)
-    //     var name = 'test';
-    //
-    //     var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
-    //
-    //     window.location.href = uri + base64(format(template, ctx))
-    // });
-</script>
 </html>
