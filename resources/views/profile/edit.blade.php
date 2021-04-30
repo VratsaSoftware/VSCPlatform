3616fef6b5579f8a5a5ed7de5f4a3868f59d9eb7
Georgi <georgidim30@gmail.com>
2021-04-30 20:24:49 +0300

    Add/Edit and Delete work experience on mobile version

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -332,25 +332,29 @@
             <div class="col pb-2">
                 <p class="fw-bold bio-title">Работен опит</p>
                 <div class="bio-description-large">
-                    <div class="row g-0">
-                        <div class="col-auto pe-3 fw-bold item-number">1.</div>
-                        <div class="col mb-3 bio-description">Lorem ipsum
+                    @foreach($allWorkExperience as $workExperience)
+                        <div class="row g-0">
+                            <div class="col-auto pe-3 fw-bold item-number">{{ $loop->iteration }}.</div>
+                            <div class="col mb-3 bio-description">
+                                <span style="font-size: 18px">
+                                    <div class="col-auto pe-3 fw-bold item-number">{{ $workExperience->Company->name }}</div>
+                                    {{ $workExperience->y_from->format('d.m.Y') }}/{{ $workExperience->y_to ? $workExperience->y_to->format('d.m.Y') : 'В ход' }}
+                                </span>
+                            </div>
+                            <span data-bs-toggle="modal" data-bs-target="#editWorkExperienceModal" class="btn position-absolute top-0 end-0 d-flex justify-content-center align-items-center edit-area-btn m-2 work-experience-btn" data-work-experience="{{ $workExperience }}" data-work-experience-company="{{ $workExperience->Company->name }}" data-work-position="{{ $workExperience->Position->position }}" data-work-y_from="{{ $workExperience->y_from->format('m/d/Y') }}" data-work-y_to="{{ $workExperience->y_to->format('m/d/Y') }}" data-work-id="{{ $workExperience->id }}">
+                                <i class="fas fa-pen"></i>
+                            </span>
+                            <form method="post" action="{{ url('/user/delete/work/' . $workExperience->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                    <button class="btn position-absolute bottom-0 end-0 d-flex justify-content-center align-items-center edit-area-btn m-2">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                            </form>
                         </div>
-                        <button data-bs-toggle="modal" data-bs-target="#workExperienceModal" class="btn position-absolute top-0 end-0 d-flex justify-content-center align-items-center edit-area-btn m-2">
-                            <i class="fas fa-pen"></i>
-                        </button>
-                    </div>
-                    <div class="row g-0">
-                        <div class="col-auto pe-3 fw-bold item-number">2.</div>
-                        <div class="col bio-description">Lorem ipsum
-                        </div>
-                        <button data-bs-toggle="modal" data-bs-target="#workExperienceModal" class="btn position-absolute top-0 end-0 d-flex justify-content-center align-items-center edit-area-btn m-2">
-                            <i class="fas fa-pen"></i>
-                        </button>
-                    </div>
-                    <!-- Modal Open Button -->
-                    <button class="bio-pill position-absolute bottom-0 start-0 m-2 p-0" data-bs-toggle="modal"
-                    data-bs-target="#workExperienceModal">+</button>
+                    @endforeach
+                    <span class="bio-pill position-absolute bottom-0 start-0 m-2 p-0"
+                    data-bs-toggle="modal" data-bs-target="#createWorkExperienceModal">+</span>
                 </div>
             </div>
             <div class="col">
